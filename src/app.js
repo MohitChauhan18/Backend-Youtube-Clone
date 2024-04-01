7df86c9b827e3cb486c9ce85e8584b9b655b1839
Harsh Gaur <76653512+HarshGaur387R@users.noreply.github.com>
2024-04-01 20:23:02 +0000

    Added error handling middleware in app.js to handle error thrown by `asyncHandler` in `ApiError` 's Object.

--- src/app.js
+++ src/app.js
@@ -40,9 +40,9 @@ app.use("/api/v1/dashboard", dashboardRouter)
 // Error middleware.
 app.use((err, req, res, next) => {
     // Log the error (you can customize this part)
-    console.error(err.stack);
+    console.error(err.errorstack);
     // Respond with an appropriate error message
-    res.status(err.status || 500).json({ success: false, message: err.message || 'Error From Server' });
+    res.status(err.statusStatus || 500).json({ success: false, message: err.message || 'Error From Server' });
 });
 
 // http://localhost:8000/api/v1/users/register
