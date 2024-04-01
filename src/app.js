e89f61bcaa3f631a7c4124302dadcad36c0ea6e9
Harsh Gaur <76653512+HarshGaur387R@users.noreply.github.com>
2024-04-02 01:20:12 +0530

    Added error handling middleware in app.js to handle error thrown by `asyncHandler` in `ApiError` 's Object.
    The middleware responsible for error handling is conspicuously absent from both the tutorial and the source code. Despite its critical role, this component remains unaddressed. I resolved this issue by adding error handling middleware in app.js file.

--- src/app.js
+++ src/app.js
@@ -9,14 +9,6 @@ app.use(cors({
     credentials: true
 }))
 
-// Error middleware.
-app.use((err, req, res, next) => {
-    // Log the error (you can customize this part)
-    console.error(err.stack);
-    // Respond with an appropriate error message
-    res.status(err.status || 500).json({ success: false, message: err.message || 'Error From Server' });
-});
-
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
@@ -44,6 +36,14 @@ app.use("/api/v1/comments", commentRouter)
 app.use("/api/v1/likes", likeRouter)
 app.use("/api/v1/playlist", playlistRouter)
 app.use("/api/v1/dashboard", dashboardRouter)
+
+// Error middleware.
+app.use((err, req, res, next) => {
+    // Log the error (you can customize this part)
+    console.error(err.stack);
+    // Respond with an appropriate error message
+    res.status(err.status || 500).json({ success: false, message: err.message || 'Error From Server' });
+});
 
 // http://localhost:8000/api/v1/users/register
 
