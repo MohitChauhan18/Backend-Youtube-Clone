99f86ab8e101ed93d72f1864393e980196f5750d
Papan Sarkar <papansrkr876@gmail.com>
2024-10-04 23:13:03 +0530

    fixed multer middeware bug

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,15 +1,49 @@
-import multer from "multer";
+import multer from "multer"
+import { User } from "../models/user.model.js"
+import { ApiError } from "../utils/ApiError.js"
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "./public/temp")
+    destination: async function (req, file, cb) {
+        const { username, email } = req.body;
+
+        // Check if the user already exists
+        const existedUser = await User.findOne({
+            $or: [{ username }, { email }]
+        });
+
+        if (existedUser) {
+            cb(new ApiError(400, "User already exists, file not stored."));
+        } else {
+            cb(null, "./public/temp");
+        }
     },
     filename: function (req, file, cb) {
-      
-      cb(null, file.originalname)
+        cb(null, file.originalname);
     }
-  })
+});
+
+export const upload = multer({ storage });
+
+
+
+
+
+
+
+// previous code
+
+// import multer from "multer";
+
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//       cb(null, "./public/temp")
+//     },
+//     filename: function (req, file, cb) {
+      
+//       cb(null, file.originalname)
+//     }
+//   })
   
-export const upload = multer({ 
-    storage, 
-})
+// export const upload = multer({ 
+//     storage, 
+// })
